289d5b7c285bc517543b0a54ab4459aa3c38ec0d
Tatyana <taa@anadeainc.com>
2024-04-07 11:09:31 +0300

    change build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val springdocVersion = "2.3.0"
 
 plugins {
-	id("org.springframework.boot") version "3.2.0-SNAPSHOT"
+	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.20"
 	kotlin("plugin.jpa") version "1.9.20"
@@ -19,15 +19,11 @@ java {
 
 repositories {
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
 	// Spring
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.modulith:spring-modulith-starter-core")
-	testImplementation("org.springframework.modulith:spring-modulith-starter-test")
 
 	// Cors
 	testImplementation("org.apache.httpcomponents.client5:httpclient5")
@@ -55,12 +51,6 @@ dependencies {
 	testImplementation("org.testcontainers:junit-jupiter")
 }
 
-dependencyManagement {
-	imports {
-		mavenBom("org.springframework.modulith:spring-modulith-bom:1.1.0-RC2")
-	}
-}
-
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs += "-Xjsr305=strict"
@@ -70,4 +60,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+	enabled = false
 }
